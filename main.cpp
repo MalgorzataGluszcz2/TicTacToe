87442eac823b64ad8be9765e15812b01f7fff9de
MalgorzataGluszcz2 <malglu.indiedeveloper@gmail.com>
2026-03-04 16:54:24 +0100

    Added game title

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include "include/TicTacToe.hpp"
 
+void gameTitle();
 void drawBoad(const Board &board);
+void theWinner(Player player, TicTacToe game);
 
 int main()
 {
 	TicTacToe game;
 	Player currentPlayer = Player::X;
 
+	gameTitle();
+
 	while (game.checkWinner() == Player::NONE && !game.isBoardFull())
 	{
 		drawBoad(game.getBoard());
+		std::cout << '\n';
 
 		if (currentPlayer == Player::X)
 		{
 			int row, col;
-			std::cout << "Enter row and column (0 - 2 0 - 2) :";
+			std::cout << "Enter row and column (0 - 2 0 - 2): ";
 			std::cin >> row >> col;
+			std::cout << '\n';
 
 			if (!game.makeMove(row, col, Player::X))
 			{
@@ -38,19 +44,17 @@ int main()
 
 	drawBoad(game.getBoard());
 
-	Player winner = game.checkWinner();
-	
-	if (winner == Player::X)
-		std::cout << "You win! Congratulations!\n";
-	else if (winner == Player::O)
-		std::cout << "AI win!\n";
-	else
-		std::cout << "Draw!\n";
+	theWinner(currentPlayer, game);
 
 	std::cin.get();
 	std::cin.get();
 
 	return 0;
+}
+
+void gameTitle()
+{
+	std::cout << "============== WELCOME TO MY GAME TIC TAC TOE ==============\n";
 }
 
 void drawBoad(const Board& board) 
@@ -74,4 +78,16 @@ void drawBoad(const Board& board)
 
 		std::cout << "\n-------------\n";
 	}
+}
+
+void theWinner(Player player, TicTacToe game)
+{
+	Player winner = game.checkWinner();
+
+	if (winner == Player::X)
+		std::cout << "You win! Congratulations!\n";
+	else if (winner == Player::O)
+		std::cout << "AI win!\n";
+	else
+		std::cout << "Draw!\n";
 }
